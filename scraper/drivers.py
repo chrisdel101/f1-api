f9b5aadf6a376ade889447704473d7855b48dcc8
Chris Del <drumgod101@gmail.com>
2019-07-19 17:11:22 -0400

    add error checking for all values when getting driver data. Increase robustnetss

--- scraper/drivers.py
+++ scraper/drivers.py
@@ -1,8 +1,10 @@
 from scraper import endpoints, utils
-
 from bs4 import BeautifulSoup
 import requests
 from user_agent import generate_user_agent
+from slugify import slugify, Slugify
+_slugify = Slugify(to_lower=True)
+
 
 headers = {
     'User-Agent': generate_user_agent(os=None, navigator=None, platform=None, device_type=None),
@@ -30,33 +32,82 @@ def _driver_images(name):
     soup = BeautifulSoup(page.text, 'html.parser')
     driver_info = soup.find(
         'figcaption', class_="driver-details")
-    return {
-        'main_image': "{0}/{1}".format(endpoints.home_endpoint(), soup.find(class_='driver-main-image').img['src']),
-        'name': driver_info.h1.string,
-        'number': driver_info.find('span').string,
-        'flag_img_url': driver_info.find_all('img')[0]['src']
-    }
+    driver_images = {}
+    try:
+        if soup.find(class_='driver-main-image') and soup.find(class_='driver-main-image').img:
+            driver_images['main_image'] = "{0}/{1}".format(
+                endpoints.home_endpoint(), soup.find(class_='driver-main-image').img['src'])
+        else:
+            print("Error: No main image for driver found.")
+
+        if driver_info.find('h1', {"class", "driver-name"}):
+            driver_images['driver_name'] = driver_info.find(
+                'h1', {"class", "driver-name"}).text
+        else:
+            print("Error: No name for driver found.")
+
+        if driver_info.find('div', {'class', 'driver-number'}):
+            driver_images['driver_number'] = driver_info.find(
+                'div', {'class', 'driver-number'}).span.text
+        else:
+            print("Error: No number for driver found.")
+
+            print("Error: No number for driver found.")
+        if driver_info.find('span', {'class', 'icn-flag'}) and driver_info.find(
+                'span', {'class', 'icn-flag'}).img.has_attr('src'):
+            driver_images['flag_img_url'] = driver_info.find(
+                'span', {'class', 'icn-flag'}).img['src']
+        else:
+            print("Error: No flag-icon for driver found.")
+            # print(driver_info.h1.has_class("driver-name"))
+            # return {
+            #     'main_image': "{0}/{1}".format(endpoints.home_endpoint(), soup.find(class_='driver-main-image').img['src']),
+            #     'name': driver_info.h1.string,
+            #     'number': driver_info.find('span').string,
+            #     'flag_img_url': driver_info.find_all('img')[0]['src']
+            #
+        return driver_images
+    except ValueError:
+        return "An error occured creating driver images."
 
 
 def driver_stats(name):
     page = requests.get(endpoints.driver_endpoint(name), headers=headers)
     soup = BeautifulSoup(page.text, 'html.parser')
     driver_details = soup.find(class_='driver-details')
-    driver_stats = driver_details.find_all('tr')[8].td.string
-    images = _driver_images(name)
-    return {
-        'main_image': images['main_image'],
-        'name': images['name'],
-        'number': images['number'],
-        'flag_img_url': images['flag_img_url'],
-        'team': driver_details.find_all('tr')[0].td.string,
-        'country': driver_details.find_all('tr')[1].td.string,
-        'podiums': driver_details.find_all('tr')[2].td.string,
-        'points': driver_details.find_all('tr')[3].td.string,
-        'grand_prix_entered': driver_details.find_all('tr')[4].td.string,
-        'world_championships': driver_details.find_all('tr')[5].td.string,
-        'highest_finish': driver_details.find_all('tr')[6].td.string,
-        'highest_grid_position': driver_details.find_all('tr')[7].td.string,
-        'DOB': driver_details.find_all('tr')[8].td.string,
-        'place_of_birth': driver_details.find_all('tr')[8].td.string
-    }
+    details = ['Team',
+               'Country',
+               'Podiums',
+               'Points',
+               'Grand Prix entered',
+               'World Championships',
+               'Higest race finish',
+               'Highest grid position',
+               'Date of birth',
+               'Place of birth'
+               ]
+    driver_dict = {}
+    _driver_images(name)
+    try:
+        for _, (k, v) in enumerate(_driver_images(name).items()):
+            driver_dict[k] = v
+    except ValueError:
+        return "An error occured unpacking driver images"
+    # error checking
+    try:
+        if driver_details.find_all('tr'):
+            # loop over html
+            for driver in driver_details.find_all('tr'):
+                # loop over all wanted details
+                for detail in details:
+                    # if they match add to driver object
+                    if driver.span and driver.span.text == detail:
+                        driver_dict[_slugify(driver.span.text)
+                                    ] = driver.td.text
+                        continue
+            return driver_dict
+    except ValueError:
+        return "An error occured creating driver data."
+        # 'main_image': images['main_image'],
+        # 'name': images['name'],
+        # 'number': images['number'],
