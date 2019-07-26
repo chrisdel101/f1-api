4634b857a6986b73f6ca8507dbed6e9e95eeb961
Chris Del <drumgod101@gmail.com>
2019-07-26 17:05:04 -0400

    some progess with getting model to save, and be in own file

--- app.py
+++ app.py
@@ -1,19 +1,24 @@
 from controllers import drivers_controller, teams_controller
 import utils
+from database import db
 from models import driver_model
 from flask import Flask
 from flask import render_template
 from flask import jsonify
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 
 app = Flask(__name__)
+db.init_app(app)
 
 app.config.from_pyfile("flask.cfg")
 app.config.update(
     SQLALCHEMY_DATABASE_URI=app.config['SQLALCHEMY_DATABASE_URI']
 )
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
+# migrate = Migrate(app, db)
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
@@ -26,52 +31,9 @@ def all_drivers():
 
 @app.route('/drivers/<driver_slug>')
 def driver(driver_slug):
-    print("HELLLLIII")
-    print("===========================")
-    print("===========================")
-    print("===========================")
-    print("===========================")
-    print("===========================")
-    print("===========================")
-    print("===========================")
-    print("===========================")
-    print("===========================")
+
     new_data = drivers_controller.driver_stats(driver_slug)
-    db.create_all()
-    __table_args__ = {'extend_existing': True}
-    db.metadata.clear()
-    if driver_model.Driver:
-        print('ZZZ', driver_model.Driver.query.all())
-        # look up by slug
-        # slug = driver_model.Driver.query.filter_by(
-        #     name_slug=driver_slug).first()
-        # if slug:
-        #     # convert to dict
-        #     db_data = dict((col, getattr(slug, col))
-        #                    for col in slug.__table__.columns.keys())
-        #     # check if any values are changed
-        #     changed_vals = utils.dict_compare_vals(new_data, db_data)
-        #     print('XX', changed_vals)
-
-        return 'Complete'
-
-    # d = driver_model.Driver(
-    #     driver_name=data['driver_name'],
-    #     name_slug=data['driver_slug'],
-    #     date_of_birth=data['date_of_birth'],
-    #     driver_number=data['driver_number'],
-    #     place_of_birth=data['place_of_birth'],
-    #     flag_img_url=data['flag_img_url'],
-    #     main_image=data['main_image'],
-    #     podiums=data['podiums'],
-    #     points=data['points'],
-    #     world_championships=data['world_championships'],
-    #     team=data['team']
-    # )
-
-    # db.session.add(d)
-    # db.session.commit()
-
+    print('DD', driver_model.Driver.create(new_data))
     return "hello"
 
 
--- models/driver_model.py
+++ models/driver_model.py
@@ -1,6 +1,4 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
+from database import db
 
 
 class Driver(db.Model):
@@ -17,7 +15,48 @@ class Driver(db.Model):
     points = db.Column(db.String(10))
     world_championships = db.Column(db.String(10))
     team = db.Column(db.String(50))
-    highest_grid_position = podiums = db.Column(db.String(10))
+    highest_grid_position = db.Column(db.String(10))
 
     def __repr__(self):
-        return "{0}".format(self.name)
+        return "{0}".format(self.driver_name)
+
+    @classmethod
+    def create(self, data):
+        db.create_all()
+        d = self(
+            driver_name=data['driver_name'],
+            name_slug=data['driver_slug'],
+            date_of_birth=data['date_of_birth'],
+            driver_number=data['driver_number'],
+            place_of_birth=data['place_of_birth'],
+            flag_img_url=data['flag_img_url'],
+            main_image=data['main_image'],
+            podiums=data['podiums'],
+            points=data['points'],
+            world_championships=data['world_championships'],
+            team=data['team']
+        )
+        db.session.add(d)
+        db.session.commit()
+        print(self.query.all())
+    # d = driver_model.Driver(
+    #     driver_name=data['driver_name'],
+    #     name_slug=data['driver_slug'],
+    #     date_of_birth=data['date_of_birth'],
+    #     driver_number=data['driver_number'],
+    #     place_of_birth=data['place_of_birth'],
+    #     flag_img_url=data['flag_img_url'],
+    #     main_image=data['main_image'],
+    #     podiums=data['podiums'],
+    #     points=data['points'],
+    #     world_championships=data['world_championships'],
+    #     team=data['team']
+    # )
+
+
+#  # convert to dict
+#         db_data = dict((col, getattr(slug, col))
+#                        for col in slug.__table__.columns.keys())
+#         # check if any values are changed
+#         changed_vals = utils.dict_compare_vals(new_data, db_data)
+#         print('XXX', changed_vals)
