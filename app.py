39bf516ad8caf9f4b701c05b1201ab8aef4f4af6
Chris Del <drumgod101@gmail.com>
2019-07-27 14:34:42 -0400

    merge

--- app.py
+++ app.py
@@ -1,9 +1,22 @@
-from controllers import drivers_controller
+from flask_sqlalchemy import SQLAlchemy
+# from database import db
+# from controllers import drivers_controller
+import utils
+from models import driver_model
+from flask import Flask
 from flask import render_template
+from flask import jsonify
+from flask_migrate import Migrate
 
 from flask import jsonify
 from flask import Flask
 app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config.from_pyfile("flask.cfg")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+# db.init_app(app)
+migrate = Migrate(app, db)
 
 
 @app.route('/drivers')
@@ -19,6 +32,29 @@ def all_drivers():
 def driver(driver):
     data = drivers_controller.driver_stats(driver)
     return jsonify(data)
+
+
+@app.route('/drivers/<driver_slug>')
+def driver(driver_slug):
+
+    new_data = drivers_controller.driver_stats(driver_slug)
+    # if doesn't exist - create
+    # if not driver_model.Driver.exists(driver_slug):
+    driver_model.Driver.create(new_data)
+    # else:
+    # else update
+    driver_model.Driver.update(new_data)
+    return "hello"
+
+
+@app.route('/teams')
+def all_teams():
+    return jsonify(teams_controller.list_all_teams())
+
+
+@app.route('/teams/<team_slug>')
+def team(team_slug):
+    return jsonify(teams_controller.team_stats(team_slug))
 
 
 if __name__ == '__main__':
---
+++ models/driver_model.py
@@ -0,0 +1,64 @@
+from database import db
+import utils
+from flask import jsonify
+from flask_migrate import Migrate
+from alembic import op
+from sqlalchemy import Column, String
+
+
+class Driver(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    driver_name = db.Column(db.String(80), nullable=False)
+    country = db.Column(db.String(100))
+    name_slug = db.Column(db.String(80), unique=True, nullable=False)
+    date_of_birth = db.Column(db.String(20))
+    driver_number = db.Column(db.String(10))
+    place_of_birth = db.Column(db.String(50))
+    flag_img_url = db.Column(db.String(150))
+    main_image = db.Column(db.String(150))
+    podiums = db.Column(db.String(10))
+    points = db.Column(db.String(10))
+    world_championships = db.Column(db.String(10))
+    team = db.Column(db.String(50))
+    highest_grid_position = db.Column(db.String(10))
+
+    def __repr__(self):
+        return "{0}".format(self.driver_name)
+
+    @classmethod
+    def create(self, data_dict):
+        db.create_all()
+        d = self(
+            driver_name=data_dict.get('driver_name'),
+            name_slug=data_dict.get('driver_slug'),
+            date_of_birth=data_dict.get('date_of_birth'),
+            driver_number=data_dict.get('driver_number'),
+            place_of_birth=data_dict.get('place_of_birth'),
+            flag_img_url=data_dict.get('flag_img_url'),
+            main_image=data_dict.get('main_image'),
+            podiums=data_dict.get('podiums'),
+            points=data_dict.get('points'),
+            world_championships=data_dict.get('world_championships'),
+            team=data_dict.get('team'),
+            country=data_dict.get('country'),
+            highest_grid_position=data_dict.get('highest_grid_position')
+        )
+        # print('d', d.name_slug)
+        db.session.add(d)
+        db.session.commit()
+
+    @classmethod
+    def update(self, data_dict):
+        # print(data_dict)
+        db_dict = utils.convert_db_row_dict(self, data_dict)
+        result = utils.dict_compare_vals(data_dict, db_dict)
+
+        op.add_column('driver',
+                      Column('TEST', String())
+                      )
+
+    @classmethod
+    def exists(self, driver_slug):
+        if self.query.filter_by(name_slug=driver_slug).first():
+            return True
+        return False
