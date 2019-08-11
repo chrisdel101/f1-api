27f9fb642c6fe977d79b5beaa5497ebeaca6b51f
Chris Del <drumgod101@gmail.com>
2019-08-11 14:59:19 -0400

    adjust tests to be more robust

--- test_app.py
+++ test_app.py
@@ -96,27 +96,31 @@ class TestDriverScraper(unittest.TestCase):
         self.assertEqual(result2['driver_number'], '8')
 
 
-@unittest.skip
+# @unittest.skip
 class TestTeamScraper(unittest.TestCase):
-    @unittest.skip
+    # @unittest.skip
     def test_scrape_all_team_names(self):
         result = team_scraper.scrape_all_team_names()
         self.assertTrue(type(result) == list)
         self.assertTrue(len(result) >= 1)
 
-    @unittest.skip
+    # @unittest.skip
     def test_get_main_image(self):
         soup = team_scraper._team_page_scrape()
-        # first one in list is currently Mercedes - will fail if markup changes
-        li = soup.find('li', {'class', 'teamindex-teamteaser'})
-        team_dict1 = {'full_team_name': 'Mercedes AMG Petronas Motorsport', 'base': 'Brackley, United Kingdom', 'team_chief': 'Toto Wolff', 'technical_chief': 'James Allison', 'power_unit': 'Mercedes', 'first_team_entry': '1970', 'highest_race_finish':
-                      '1 (x87)', 'pole_positions': '101', 'fastest_laps': '61', 'name_slug': 'mercedes', 'url_name_slug': 'Mercedes'}
-        team_dict2 = {'full_team_name': 'ROKiT Williams Racing', 'base': 'Grove, United Kingdom', 'team_chief': 'Frank Williams', 'technical_chief': 'TBC', 'power_unit': 'Mercedes',
-                      'first_team_entry': '1978', 'highest_race_finish': '1 (x114)', 'pole_positions': '129', 'fastest_laps': '133', 'name_slug': 'williams', 'url_name_slug': 'Williams'}
-        d1 = team_scraper.get_main_image(team_dict1, li, 'Mercedes')
-        self.assertTrue('main_image' in d1)
-        d2 = team_scraper.get_main_image(team_dict2, li, 'Williams')
-        self.assertTrue('main_image' in d2)
+        ferrariList = get_team_list('Ferrari', soup)
+        ferrari_data = {
+            'url_name_slug': "Ferrari",
+        }
+        team_scraper.get_main_image(
+            ferrari_data, ferrariList, 'Ferrari')
+        self.assertTrue('main_image' in ferrari_data)
+        williamsList = get_team_list('Williams', soup)
+        williams_data = {
+            'url_name_slug': "Williams",
+        }
+        team_scraper.get_main_image(
+            williams_data, williamsList, 'Williams')
+        self.assertTrue('main_image' in williams_data)
 
     # @unittest.skip
     def test_get_driver_flag_url(self):
@@ -126,19 +130,19 @@ class TestTeamScraper(unittest.TestCase):
             'url_name_slug': 'Williams'
         }
         team_scraper.get_flag_img_url(williams_dict, williamsList, 'Williams')
-        print(williams_dict)
         self.assertTrue('flag_img_url' in williams_dict)
 
-    @unittest.skip
+    # @unittest.skip
     def test_get_logo_url(self):
         soup = team_scraper._team_page_scrape()
-        li = soup.find('li', {'class', 'teamindex-teamteaser'})
-        team_dict1 = {'full_team_name': 'Rich Energy Haas F1 Team', 'base': 'Kannapolis, United States', 'team_chief': 'Guenther Steiner', 'technical_chief': 'Rob Taylor', 'power_unit': 'Ferrari', 'first_team_entry': '2016', 'highest_race_finish':
-                      '4 (x1)', 'pole_positions': 'N/A', 'fastest_laps': '1', 'name_slug': 'haas_f1_team', 'url_name_slug': 'Haas', 'main_image': 'https://www.formula1.com//content/fom-website/en/teams/Haas/_jcr_content/image16x9.img.1536.medium.jpg/1561196026039.jpg', 'flag_img_url': 'https://www.formula1.com//content/fom-website/en/teams/Haas/_jcr_content/countryFlag.img.jpg/1422627086486.jpg'}
-        team_scraper.get_logo_url(team_dict1, li, 'Haas')
-        self.assertTrue('logo_url' in team_dict1)
+        haasList = get_team_list('Haas', soup)
+        haas_data = {
+            'url_name_slug': 'Haas'
+        }
+        team_scraper.get_logo_url(haas_data, haasList, 'Haas')
+        self.assertTrue('logo_url' in haas_data)
 
-    @unittest.skip
+    # @unittest.skip
     def test_get_championship_titles(self):
         soup = team_scraper._team_page_scrape()
         racingPointList = get_team_list('Racing-Point', soup)
@@ -148,7 +152,6 @@ class TestTeamScraper(unittest.TestCase):
         team_scraper.get_championship_titles(
             racing_point_data, racingPointList, 'Racing-Point')
         self.assertTrue('championship_titles' in racing_point_data)
-        # scrape only mercedes data
         MercedesList = get_team_list('Mercedes', soup)
         mercedes_data = {
             'url_name_slug': "Mercedes"
@@ -157,16 +160,15 @@ class TestTeamScraper(unittest.TestCase):
             mercedes_data, MercedesList, 'Mercedes')
         self.assertTrue('championship_titles' in mercedes_data)
 
-    @unittest.skip
+    # @unittest.skip
     def test_get_podium_finishes(self):
         soup = team_scraper._team_page_scrape()
-        li = soup.find('li', {'class', 'teamindex-teamteaser'})
-        team_dict1 = {'full_team_name': 'Rich Energy Haas F1 Team', 'base': 'Kannapolis, United States', 'team_chief': 'Guenther Steiner', 'technical_chief': 'Rob Taylor', 'power_unit': 'Ferrari', 'first_team_entry': '2016', 'highest_race_finish':
-                      '4 (x1)', 'pole_positions': 'N/A', 'fastest_laps': '1', 'name_slug': 'haas_f1_team', 'url_name_slug': 'Haas', 'main_image': 'https://www.formula1.com//content/fom-website/en/teams/Haas/_jcr_content/image16x9.img.1536.medium.jpg/1561196026039.jpg', 'flag_img_url': 'https://www.formula1.com//content/fom-website/en/teams/Haas/_jcr_content/countryFlag.img.jpg/1422627086486.jpg'}
-        team_scraper.get_podium_finishes(team_dict1, li, 'Haas')
-        self.assertTrue('podium_finishes' in team_dict1)
-
-    # @unittest.skip
+        renaultList = get_team_list('Renault', soup)
+        renault_data = {
+            'url_name_slug': 'Haas'
+        }
+        team_scraper.get_podium_finishes(renault_data, renaultList, 'Haas')
+        self.assertTrue('podium_finishes' in renault_data)
 
 
 class TestUtils(unittest.TestCase):
